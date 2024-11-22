99b35a781f4ba1de2fc52d805840d76268e2b4a9
SilverofLight <a2061686759@outlook.com>
2024-11-22 21:32:49 +0800

    del 0->-

--- lua/config/neovide.lua
+++ lua/config/neovide.lua
@@ -2,5 +2,4 @@ if vim.g.neovide then
   vim.o.guicursor = "n-v-c:block-Cursor,i-ci:ver25-Cursor"
   vim.o.guifont = "Source Code Pro:h10"
   vim.cmd([[highlight Cursor guifg=#282A36 guibg=#cab6f7]])
-  vim.keymap.set("i","0","-")
 end
