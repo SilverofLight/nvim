db5b0533248e45b43ba3642dc6d0b4f7dcc7e7af
SilverofLight <a2061686759@outlook.com>
2024-12-30 20:17:11 +0800

    delete markdown_shortkey

--- init.lua
+++ init.lua
@@ -2,7 +2,6 @@ require("config.keymaps")
 require("config.lazy")
 require("config.options")
 require("config.cursor")
-require("config.markdown_shortkey")
 require("nvim-tree").setup()
 require("lualine").setup {
   options = { theme = 'dracula-nvim' },
